c7839517cf8418ae566d607bae2c946384df8b64
Stanislav Obrazcoff <obrazcoff@gmail.com>
2019-01-08 15:21:54 +0100

    Updates

---
+++ precache-manifest.60291ee93292298075d317bc9e8e74de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46aed914ca17434841c9",
+    "url": "/HotelFilter/static/js/runtime~main.46aed914.js"
+  },
+  {
+    "revision": "4ebcda96b1c14117afc3",
+    "url": "/HotelFilter/static/js/main.4ebcda96.chunk.js"
+  },
+  {
+    "revision": "8f43bd86b838f237a629",
+    "url": "/HotelFilter/static/js/1.8f43bd86.chunk.js"
+  },
+  {
+    "revision": "4ebcda96b1c14117afc3",
+    "url": "/HotelFilter/static/css/main.5d811f07.chunk.css"
+  },
+  {
+    "revision": "1d472bbd89c67947024511fedd93d5c6",
+    "url": "/HotelFilter/index.html"
+  }
+];
